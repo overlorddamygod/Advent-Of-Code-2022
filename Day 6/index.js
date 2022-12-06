95f088bf96de6e2fee96295ce39be6152a030cce
Overlord <overlord.damygod@gmail.com>
2022-12-06 14:13:07 +0545

    Day 6 donezo 👏

---
+++ Day 6/index.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", "utf8", function (err, input) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Part 1: ${part1(input)}`);
+  console.log(`Part 2: ${part2(input)}`);
+});
+
+// Part 1
+// No. of distinct chars = 4
+const part1 = (input) => {
+  let start = 0;
+  let end = 0;
+  let chopped = 0;
+  
+  let seen = {}
+
+  while ( input.length > 0 ) {
+    const char = input[end]
+
+    if (!Object.keys(seen).includes(char)) {
+      end++
+      seen[char] = true;
+
+      if (end - start == 4) {
+        return end
+      }
+    } else {
+      const index = input.slice(start).indexOf(char) 
+      chopped += index + 1
+      
+      start = chopped;
+      end = chopped;
+
+      seen = {}
+    }
+  }
+}
+
+// Part 2
+// Same as Part 1 but only difference is no. of distinct characters... Can be extracted to function with input as no. of chars
+// No. of distinct chars = 14
+const part2 = (input) => {
+  let start = 0;
+  let end = 0;
+  let chopped = 0;
+  
+  let seen = {}
+
+  while ( input.length > 0 ) {
+    const char = input[end]
+
+    if (!Object.keys(seen).includes(char)) {
+      end++
+      seen[char] = true;
+
+      if (end - start == 14) {
+        return end
+      }
+    } else {
+      const index = input.slice(start).indexOf(char) 
+      chopped += index + 1
+      
+      start = chopped;
+      end = chopped;
+
+      seen = {}
+    }
+  }
+}
