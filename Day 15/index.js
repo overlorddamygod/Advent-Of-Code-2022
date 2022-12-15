242b12fc3044e6b7d123bf4aaf916719130cac28
Overlord <overlord.damygod@gmail.com>
2022-12-15 20:29:21 +0545

    Day 15 Part 2 Donezo.. and some optimizations

--- Day 15/index.js
+++ Day 15/index.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
 
-fs.readFile("input.txt", "utf8", function (err, input) {
-  if (err) {
-    return console.log(err);
-  }
-  const sampleY = 10;
-  console.log(`Part 1: ${part1(input, sampleY)}`);
-  // console.log(`Part 2: ${part2(input)}`);
-});
+const manhattanDistance = (a, b) => {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+};
 
 const parseInput = (input) => {
   const coords = input.split("\n").map((line) => {
@@ -15,11 +10,10 @@ const parseInput = (input) => {
       /^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$/.exec(
         line
       );
+    const sensor = [+sx, +sy];
+    const beacon = [+bx, +by];
 
-    return [
-      [+sx, +sy],
-      [+bx, +by],
-    ];
+    return [sensor, beacon, manhattanDistance(sensor, beacon)];
   });
 
   const grid = {};
@@ -38,23 +32,18 @@ const parseInput = (input) => {
 const part1 = (input, column) => {
   let { coords, grid } = parseInput(input);
 
-  const manhattanDistance = (a, b) => {
-    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-  };
-
   const set = (coord) => {
     // if (coord[1] != column) return;
     if (grid[coord]) return;
     grid[coord] = "#";
-    count++
+    count++;
   };
 
   let count = 0;
 
   for (let i = 0; i < coords.length; i++) {
-    console.log("COORD", coords[i]);
-    const [sensor, beacon] = coords[i];
-    const distance = manhattanDistance(sensor, beacon);
+    // console.log("COORD", coords[i]);
+    const [sensor, beacon, distance] = coords[i];
 
     const yU = sensor[1] - distance;
     const yD = sensor[1] + distance;
@@ -69,8 +58,8 @@ const part1 = (input, column) => {
     for (let xd = 0; xd < range; xd++) {
       const xL = sensor[0] - xd;
       set([xL, column]);
-      
-      if (xd == 0) continue
+
+      if (xd == 0) continue;
       const xR = sensor[0] + xd;
       set([xR, column]);
     }
@@ -93,84 +82,67 @@ const part1 = (input, column) => {
 };
 
 // Part 2
-// TODO
-const part2 = (input) => {
-  let { coords, grid } = parseInput(input);
-
-  const manhattanDistance = (a, b) => {
-    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
-  };
-
-  const y = 10;
-  const gridCount = {};
-  const set = (coord) => {
-    // if (coord[1] != y) return;
-    if (grid[coord] == "S" || grid[coord] == "B") return;
-    grid[coord] = "#";
-    if (coord[0] >= 0 && coord[0] <= 20 && coord[1] >= 0 && coord[1] <= 20)
-      gridCount[coord] = gridCount[coord] ? gridCount[coord] + 1 : 1;
-  };
+const part2 = (input, upperBound) => {
+  let { coords } = parseInput(input);
 
   for (let i = 0; i < coords.length; i++) {
-    console.log("COORD", coords[i]);
-    const [sensor, beacon] = coords[i];
-    const distance = manhattanDistance(sensor, beacon);
+    // console.log("COORD", coords[i]);
+    const [sensor, beacon, distance] = coords[i];
+    const borders = [];
 
+    // get All the border points
     for (let yd = 0; yd < distance + 2; yd++) {
       const yU = sensor[1] - yd;
       const yD = sensor[1] + yd;
-      console.log(yU, yD);
-      // if (yU != y && -yU != y && yD != y && -yD != y) continue;
-      // console.log(yU,yD)
-      // console.log(yU, yD)
+
       const outDistance = distance - yd + 1;
       const xr = sensor[0] + outDistance;
       const xl = sensor[0] - outDistance;
 
-      set([xl, yU]);
-      set([xl, yD]);
-      set([xr, yU]);
-      set([xr, yD]);
-      // set([-xl, -yU])
-      // set([-xl, -yD])
-      // set([-xr, -yU])
-      // set([-xr, -yD])
-      // console.log([xl, yU])
-      // console.log([xl, yD])
-      // console.log([xr, yU])
-      // console.log([xr, yD])
-      // for (let xd = 0; xd < distance - yd + 1; xd++) {
-      //   // console.log(yd,xd)
-      //   const xL = sensor[0] - xd;
-      //   const xR = sensor[0] + xd;
-
-      //   set([xL, yU]);
-      //   set([xL, yD]);
-      //   set([xR, yU]);
-      //   set([xR, yD]);
-
-      //   set([-xL, -yU]);
-      //   set([-xL, -yD]);
-      //   set([-xR, -yU]);
-      //   set([-xR, -yD]);
-      // }
+      borders.push(
+        ...[
+          [xl, yU],
+          [xl, yD],
+          [xr, yU],
+          [xr, yD],
+        ].filter((coord) => {
+          return (
+            coord[0] >= 0 &&
+            coord[0] <= upperBound &&
+            coord[1] >= 0 &&
+            coord[1] <= upperBound
+          );
+        })
+      );
     }
-  }
 
-  for (let i = 0; i <= 20; i++) {
-    let output = "" + String(i).padStart(3, " ") + "  ";
-    for (let j = 0; j <= 20; j++) {
-      if (grid[[j, i]]) {
-        output += grid[[j, i]];
-      } else {
-        output += ".";
+    // for each border points, check if they are within the range of any sensor
+    // if a point is not within any of the sensor, the point is the beacon
+    for (let point of borders) {
+      // point = point.split(",").map((a) => +a);
+      let isBeacon = true;
+      for (const [sensor, beacon, distance] of coords) {
+        if (manhattanDistance(sensor, point) <= distance) {
+          isBeacon = false;
+          break;
+        }
+      }
+      if (isBeacon) {
+        return point[0] * 4000000 + point[1];
       }
     }
-    console.log(output);
   }
-
-  console.log(gridCount);
-  return Object.keys(gridCount).filter((coord) => {
-    return gridCount[coord] == 1;
-  });
 };
+
+const solve = (filename, y, upperbound) => {
+  const input = fs.readFileSync(filename, "utf-8");
+
+  console.log(`Part 1: ${part1(input, y)}`);
+  console.log(`Part 2: ${part2(input, upperbound)}`);
+};
+
+console.log("------- Sample -------");
+solve("./sample.txt", 10, 20);
+
+console.log("------- Input -------");
+solve("./input.txt", 10, 20);
